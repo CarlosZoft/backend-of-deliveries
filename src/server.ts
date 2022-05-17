87f29a76fc98957d10ba38b41609ea43c46ed5d9
CarlosZoft <carlosk2gunn@hotmail.com>
2022-05-16 23:43:38 -0300

    ✨ feat: add createUserCase

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
+import { routes } from './routes'
 const port = 3030
 
 const app = express()
+app.use(express.json())
 
+app.use(routes)
 
 app.listen(port, () => console.log(`Server is running on port ${port}`))
