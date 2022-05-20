067b5940825018ecd99d16caec5fbe335fea0638
CarlosZoft <carlosk2gunn@hotmail.com>
2022-05-19 23:14:22 -0300

    ✨ feat: added middleware for treatment errors

--- src/server.ts
+++ src/server.ts
@@ -8,4 +8,17 @@ app.use(express.json())
 
 app.use(routes)
 
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+        return response.status(400).json({
+            message: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal server Error"
+    })
+})
+
 app.listen(port, () => console.log(`Server is running on port ${port}`))
