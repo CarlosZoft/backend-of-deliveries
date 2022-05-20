9ec634a4ac2b93bbea965f87554b58698d63596f
CarlosZoft <carlosk2gunn@hotmail.com>
2022-05-19 23:08:39 -0300

    ✨ feat: create authenticate use case

---
+++ src/modules/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { prisma } from "../../database/prismaClient";
+import { compare } from 'bcrypt'
+import { sign } from 'jsonwebtoken'
+
+
+interface IAuthenticateClient {
+    username: string;
+    password: string;
+}
+
+export class AuthenticateClientUseCase {
+    async execute({ username, password }: IAuthenticateClient) {
+
+        const client = await prisma.clients.findFirst({
+            where: {
+                username
+            }
+        })
+
+        if (!client) {
+            throw new Error("Username or password invalid!")
+        }
+
+        const passwordMatch = await compare(password, client.password)
+
+        if (!passwordMatch) {
+            throw new Error("Username or password invalid!")
+        }
+    
+        const token = sign({ username }, "0a2a47acde0606bc47e37e9f81ef83c0", {
+            subject: client.id,
+            expiresIn: "1d"
+        })
+
+        return token
+    }
+}
